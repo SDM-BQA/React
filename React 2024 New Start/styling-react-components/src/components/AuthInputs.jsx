578a13022b88e59080d79921e47a9c2685a45ae8
SDM-BQA <sdmbqa420@gmail.com>
2024-02-06 23:01:34 +0530

    input styling

--- React 2024 New Start/styling-react-components/src/components/AuthInputs.jsx
+++ React 2024 New Start/styling-react-components/src/components/AuthInputs.jsx
@@ -34,8 +34,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-slate-700 to-stone-800 mx-auto">
+      <div className="flex-col gap-2 mb-6">
           <CustomInput
           label="Email"
             type="email"
@@ -53,12 +53,12 @@ export default function AuthInputs() {
               handleInputChange("password", event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
         </Button>
       </div>
