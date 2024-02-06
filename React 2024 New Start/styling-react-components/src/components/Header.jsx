e088fb395654ce32437cafec5f2cd328cf0a9bc9
SDM-BQA <sdmbqa420@gmail.com>
2024-02-06 22:34:47 +0530

    header styling

--- React 2024 New Start/styling-react-components/src/components/Header.jsx
+++ React 2024 New Start/styling-react-components/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 // import styles from "./Header.module.css";
 import { styled } from "styled-components";
 
-const StyledHeader =styled.header `
+/*const StyledHeader =styled.header `
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,13 +46,14 @@ const StyledHeader =styled.header `
   }
   
 `;
+*/
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain"/>
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
   );
 }
